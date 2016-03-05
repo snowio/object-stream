ea5581d6d9a4a5c71f0e05e10fe19440c7d8c5b6
Josh Di Fabio <joshdifabio@gmail.com>
2016-03-05 11:23:06 +0000

    Remove debugging

--- src/WritableObjectStreamTrait.php
+++ src/WritableObjectStreamTrait.php
@@ -77,9 +77,6 @@ trait WritableObjectStreamTrait
         }
 
         if ($this->pendingItemCount >= $this->pendingItemLimit) {
-            if ($this->pendingItemLimit === 49) {
-                error_log('stream is at concurrency limit',4);
-            }
             $this->notifyDrain = true;
             return false;
         }
